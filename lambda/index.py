eb4f6af24b852f899731d2ee020670275ada30eb
Adam Divall <adivall@sky.com>
2024-01-26 10:37:38 +0000

    Removed WhiteSpace

--- lambda/index.py
+++ lambda/index.py
@@ -1,8 +1,8 @@
-import boto3 
+import boto3
 import cfnresponse
 import os
 import sys
-from botocore.exceptions import ClientError 
+from botocore.exceptions import ClientError
 
 EnvMaxPasswordAge = os.environ['MAXPASSWORDAGE']
 EnvMinPasswordLength = os.environ['MINPASSWORDLENGTH']
@@ -12,12 +12,12 @@ EnvRequireNumbers = os.environ['REQUIRENUMBERS']
 EnvRequireSymbols = os.environ['REQUIRESYMBOLS']
 EnvPasswordReusePrevention = os.environ['PASSWORDREUSEPREVENTION']
 
-iam_client = boto3.client('iam') 
+iam_client = boto3.client('iam')
 
-def lambda_handler(event,context): 
-    props = event['ResourceProperties'] 
-    if (event['RequestType'] == 'Create' or event['RequestType'] == 'Update'): 
-        try: 
+def lambda_handler(event,context):
+    props = event['ResourceProperties']
+    if (event['RequestType'] == 'Create' or event['RequestType'] == 'Update'):
+        try:
             iam_client.update_account_password_policy(
                 AllowUsersToChangePassword=True,
                 HardExpiry=False,
@@ -28,21 +28,21 @@ def lambda_handler(event,context):
                 RequireSymbols=bool(EnvRequireSymbols),
                 RequireUppercaseCharacters=bool(EnvRequireUppercaseChars),
                 PasswordReusePrevention=int(EnvPasswordReusePrevention)
-            )  
-        except ClientError as ex: 
-            print(ex.response['Error']['Message']) 
-            print("Response: FAILED") 
-            cfnresponse.send(event, context, cfnresponse.FAILED, ex.response) 
+            )
+        except ClientError as ex:
+            print(ex.response['Error']['Message'])
+            print("Response: FAILED")
+            cfnresponse.send(event, context, cfnresponse.FAILED, ex.response)
 
-    elif (event['RequestType'] == 'Delete'):        
-        try: 
+    elif (event['RequestType'] == 'Delete'):
+        try:
             iam_client.delete_account_password_policy()
-        except ClientError as ex: 
-            print(ex.response['Error']['Message']) 
-            print("Respond: FAILED") 
-            
-            cfnresponse.send(event, context, cfnresponse.FAILED, ex.response) 
-        
-    print("Response: SUCCESS") 
-    cfnresponse.send(event, context, cfnresponse.SUCCESS, {}) 
+        except ClientError as ex:
+            print(ex.response['Error']['Message'])
+            print("Respond: FAILED")
+
+            cfnresponse.send(event, context, cfnresponse.FAILED, ex.response)
+
+    print("Response: SUCCESS")
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
     sys.exit(0)
